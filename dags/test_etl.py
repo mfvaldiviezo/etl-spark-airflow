ffaf270a09701fb52d5c3d26e83d7e8a435363ff
Marcelo Valdiviezo Condolo <mfvaldiviezo@gmail.com>
2026-03-25 14:16:25 -0500

    agrego modificacion de archivo test_etl.py

--- dags/test_etl.py
+++ dags/test_etl.py
@@ -4,3 +4,7 @@ from datetime import datetime
 
 with DAG(dag_id='test_etl',start_date=datetime(2026, 2, 1), schedule_interval=None, catchup=False) as dag:
     prueba = BashOperator(task_id='presentar_mensaje', bash_command="echo 'ESTO ES UNA PRUEBA---'")
+
+
+    # Esto es el resultado de algunos cambios
+    print("Mensaje de prueba")
